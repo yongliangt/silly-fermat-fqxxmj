f55f3eef813c9d43e9c56809edb7115227db937b
Yong Liang T <yongliang99@gmail.com>
2023-08-17 00:04:25 +0800

    done

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
+#Task2
+$unstructured = file_get_contents("unstructured.txt");
 
-$unstructured = fopen("unstructured.txt", "r");
+$line = explode("\n", $unstructured);
 
-while($line = fgets($unstructured)){
-    print_r(explode("#",$line));
-    echo copy()
+$cities = array();
+
+foreach($line as $l){
+    $data = explode("#", $l);
+    if (count($data) >= 3){
+        $city = trim($data[2]);
+        $cities[] = $city;
+    }
 }
+
+file_put_contents("all_cities.txt", implode("\n", $cities));
+
+$unique_cities = array_unique($cities);
+
+file_put_contents("unique_cities.txt", implode("\n", $unique_cities));
+
+$abbreviations = array();
+
+foreach($unique_cities as $city){
+    $abbreviation = strtoupper(substr($city, 0, 3));
+    $abbreviations[] = $abbreviation;
+}
+
+file_put_contents("abbreviations.txt", implode("\n", $abbreviations));
 
 ?>
